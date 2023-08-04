c0fcb5a7954f39ac7f469a2da4a60e14b7e0c45f
ellafreudenberger <126198732+ellafreudenberger@users.noreply.github.com>
2023-08-04 00:57:26 -0700

    mongouri edits

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -2,6 +2,47 @@ const express = require('express');
 const Book = require('../models/bookModel');
 const booksRouter = express.Router();
 
+// Seed route to add initial data to the database
+booksRouter.get('/seed', (req, res) => {
+    Book.insertMany([
+      {
+        "title": "The Shinobi Initiative",
+        "description": "The reality-bending adventures of a clandestine service agency in the year 2166",
+        "year": 2014,
+        "quantity": "10",
+        "imageURL": "https://imgur.com/LEqsHy5.jpeg"
+      },
+      {
+        "title": "Tess the Wonder Dog",
+        "description": "The tale of a dog who gets super powers",
+        "year": 2007,
+        "quantity": "3",
+        "imageURL": "https://imgur.com/cEJmGKV.jpg"
+      },
+      {
+        "title": "The Annals of Arathrae",
+        "description": "This anthology tells the intertwined narratives of six fairy tales.",
+        "year": 2016,
+        "quantity": "8",
+        "imageURL": "https://imgur.com/VGyUtrr.jpeg"
+      },
+      {
+        "title": "Wâˆ€RP",
+        "description": "A time-space anomaly folds matter from different points in earth's history in on itself, sending six unlikely heroes on a race against time as worlds literally collide.",
+        "year": 2010,
+        "quantity": "4",
+        "imageURL": "https://imgur.com/qYLKtPH.jpeg"
+      }
+    ])
+    .then(() => {
+      res.status(200).json({ message: 'Seed successful' });
+    })
+    .catch((err) => {
+      res.status(400).json({ message: 'Seed unsuccessful', error: err.message });
+    });
+  });
+  
+
 // GET /books
 booksRouter.get('/', (req, res) => {
     Book.find()
