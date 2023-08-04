2cb353665044dabc8b330f3ee8cba7111fb6ed35
ellafreudenberger <126198732+ellafreudenberger@users.noreply.github.com>
2023-08-04 01:35:01 -0700

    cors

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-require('dotenv').config(); // Load environment variables from .env file
+const cors = require('cors'); // Import the CORS package
+require('dotenv').config();
 
 const app = express();
 const port = 3000;
 
 const booksRouter = require('./controllers/booksController');
 
-app.use('/books', booksRouter);
-
 // Configure body-parser for JSON
 app.use(express.json());
 
-// Connect to MongoDB
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useUnifiedTopology', true);
+// Enable CORS for all requests
+app.use(cors());
 
+// Set up books controller router
+app.use('/books', booksRouter);
+
+// Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('Connected to MongoDB');
@@ -27,13 +29,8 @@ mongoose.connect(process.env.MONGO_URI)
   .catch((err) => {
     console.error('Error connecting to MongoDB:', err.message);
   });
-  
+
 // Root index route
 app.get('/', (req, res) => {
   res.send('Hello World!');
-});
-
-// Start the server and listen on the specified port
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
 });
