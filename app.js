ca5d6af4887931adba06d49d6d3344f8e6cb98e4
ellafreudenberger <126198732+ellafreudenberger@users.noreply.github.com>
2023-08-04 00:24:09 -0700

    mongouri

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+require('dotenv').config(); // Load environment variables from .env file
+
+const app = express();
+const port = 3000;
+
+// Configure body-parser for JSON
+app.use(express.json());
+
+// MongoDB connection setup
+const mongoURI = process.env.MONGO_URI;
+
+mongoose
+  .connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err.message);
+  });
+
+// Root index route
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Start the server and listen on the specified port
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
